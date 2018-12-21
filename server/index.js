dd4d3554d5481c57c511e8cad9de4888060efac4
unknown <gabranches@gmail.com>
2018-12-21 16:15:53 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+var cors = require('cors')
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
+app.use(cors());
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
